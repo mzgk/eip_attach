b35a1c98f0fac78708d2af31ae2d685c28c26678
mzgk <mzgkworks@gmail.com>
2017-07-16 22:46:36 +0900

    end: 作成完了（デバックコードあり）

--- eip_attach.sh
+++ eip_attach.sh
@@ -4,26 +4,27 @@
 #　自分にEIPがアタッチされていなければ、プール分からアタッチする。
 # 起動時だと複数台でEIPを取り合うので、バッティングしてアタッチに失敗する場合がある。
 # なので、cronを使って間隔で処理をする。
-# eip_attached.txtの有無で判断するのは、無駄なcurlを減らすため。
+# アタッチ済みファイル（eip_attached.txt）の有無で判断するのは、無駄なcurlを減らすため。
 #
-# - /home/ec2-user/eip_attached.txtがあるか（初回は存在しない）
+# - アタッチ済みファイルがあるか（初回は存在しない）
 #   - Yes : EIPアタッチ済みなので終了
 #   - No : 未アタッチなので次の処理へ
 # - 自分にEIPがアタッチされているか（再検査）
-#   - Yes : /ec2-user/home/eip_attached.txtを作成して終了
-#   - No : 次へ
-# - プールから１つ取得
-#   - 取得したIPが他にアタッチされているか
-#     - Yes : 次のプールIPへ
-#     - No : 自分にアタッチして、/ec2-user/home/eip_attached.txtを作成して終了
+#   - Yes : アタッチ済みファイルを作成して終了
+#   - No : 未アタッチなので次の処理へ
+# - プール内から、他EC2にアタッチされていないEIPを全て取得する
+# - 一つづつ取り出し、自身にアタッチ
+#   - Yes : アタッチ済みファイルを作成し、終了
+#   - No : 次のEIPへ
 #---
 
-EIP_LIST=""
-EIP_TXT="/home/ec2-user/eip_attached.txt"
+EIP_POOL="eipalloc-8f4affeb eipalloc-bb3782df"
+ATTACHED_TXT="/home/ec2-user/eip_attached.txt"
 
 
-# /home/ec2-user/eip_attached.txtが存在するか
-if [ -f ${EIP_TXT} ]; then
+# アタッチ済みファイルが存在するか
+if [ -f ${ATTACHED_TXT} ]; then
+  echo "アタッチ済みファイルが存在"
   exit 0
 fi
 
@@ -36,10 +37,39 @@ export AWS_DEFAULT_REGION=${REGION}
 INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 # 自分のEIPを取得
-EIP=$(aws ec2 describe-addresses --filters "Name=instance-id,Values=${INSTANCE_ID}" --query "Addresses[].[AssociationId, AllocationId, PublicIp]" --output text)
+EIP=$(aws ec2 describe-addresses --filters "Name=instance-id,Values=${INSTANCE_ID}" --query "Addresses[].[PublicIp]" --output text)
 
 # アタッチされている
-if [ -n ${EIP} ]; then
-  touch ${EIP_TXT}
+if [ -n "$EIP" ]; then
+  echo "EIPアタッチ済み"
+  touch ${ATTACHED_TXT}
   exit 0
 fi
+
+echo "EIPなし"
+echo ${EIP_POOL}
+
+# プール内から、アタッチされていないEIPをすべて取得する
+NOT_ATTACHED_EIPS=$(aws ec2 describe-addresses --allocation-ids ${EIP_POOL} --filter "Name=domain,Values=vpc" --output text | grep -v eipassoc- | awk '{print $2}')
+echo ${NOT_ATTACHED_EIPS}
+
+# 一つづつ取り出し、自身にアタッチさせる
+for ALLOC_ID in ${NOT_ATTACHED_EIPS}
+do
+  echo ${ALLOC_ID}
+  CMD="aws ec2 associate-address --instance-id ${INSTANCE_ID} --allocation-id ${ALLOC_ID}"
+  $CMD
+  STATUS=$?
+  echo $STATUS
+
+  # OKならアタッチファイルを作成し、終了
+  if [ 0 = ${STATUS} ]; then
+    EIP=$(aws ec2 describe-addresses --filters "Name=instance-id,Values=${INSTANCE_ID}" --query "Addresses[].[PublicIp]" --output text)
+    echo ${EIP}
+    touch ${ATTACHED_TXT}
+    exit 0
+  fi
+done
+
+echo "EIPアタッチエラー"
+exit 1
